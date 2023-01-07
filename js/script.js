1f708a14ba15c8e0bf725e6195db168eb6d3a967
sanchopuri <112719289+sanchopuri@users.noreply.github.com>
2023-01-07 21:27:35 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,131 @@
+"use strict";
+
+
+
+
+
+//вариант 1 - Коля
+
+
+//вариант 2 - не верно
+// надо поставить ; после закрывающей скобки функции
+
+//вариант 3 - не верно
+// запуск функции нельзя писать перед функционалбном вырожением
+
+//вариант 4 
+
+/*
+let showMessage;
+if (2 > 1) {
+	showMessage = function () {
+		console.log('cogh');
+	};
+}
+
+showMessage();
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//вариант 1 - 4 слова
+
+
+//вариант 2 
+
+/*
+let users = ['Ваня','Иштван',];
+users.push ('Оля');
+console.log(users);
+users.splice (1,1, 'Петя');
+console.log(users);
+
+let user = users.slice(0,1);
+console.log(user);
+user.unshift('Маша','Паша');
+console.log(user);
+*/
+
+
+//вариант 3 
+/*
+let arr = ['Ваня','Иштван','Оля',];
+let str = arr.join(',');
+console.log(str);
+*/
+
+//вариант 4 
+/*
+let str = 'Ваня,Иштван,Оля';
+let arr = str.split(',');
+console.log(arr);
+*/
+
+
+//вариант 5 - равен первому числу в массиве
+
+
+
+
+
+
+
+let testText = document.querySelector('.page');
+
+testText.setAttribute('data-say-hi', 'yes');
+console.log(testText);
+
+
+//задача 2
+/*
+console.log(elems[2]);
+*/
+
+//задача 3
+
+/* const elems = document.getElementsByClassName('like');
+console.log(elems);
+ */
+
+//задача 4
+// объект добавится в конец списка в HTML
+
+
+
+
+
+
+
+
+
+let text = document.querySelector('.page');
+text.setAttribute('data-say-hi', 'yes');
+console.log(text);
+
+//задача 2
+const list = document.getElementsByClassName('list');
+console.log(list[0]);
+
+
+//задача 3
+const elems = document.getElementsByClassName('like');
+console.log(elems);
+
+
+//задача 4
+// объект добавится в конец списка в HTML
+
+
+
